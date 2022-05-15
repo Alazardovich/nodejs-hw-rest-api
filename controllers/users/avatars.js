f6d42837c796259c7ab1946d2fe3853f9d7e8256
Artem <lazarazuy@gmail.com>
2022-05-15 13:37:06 +0300

    +name

--- controllers/users/avatars.js
+++ controllers/users/avatars.js
@@ -7,12 +7,13 @@ const toPublic = path.join(__dirname, "../../", "public", "avatars");
 
 const changeAvatar = async (req, res) => {
   const { path: fromTmp, originalname } = req.file;
-  const resultUpload = path.join(toPublic, originalname);
+  const nameImg = `${req.user._id}_${originalname}`;
+  const resultUpload = path.join(toPublic, nameImg);
   try {
     const image = await Jimp.read(fromTmp);
     await image.resize(250, 250).writeAsync(fromTmp);
     await fs.rename(fromTmp, resultUpload);
-    const avatarURL = path.join("public", "avatars", originalname);
+    const avatarURL = path.join("public", "avatars", nameImg);
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
     res.status(200).json({ avatarURL });
   } catch (error) {
